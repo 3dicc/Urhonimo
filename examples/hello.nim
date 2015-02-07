36f994d8cec61f034513931d824fd62aed9ece1e
Andreas Rumpf <rumpf_a@web.de>
2015-02-07 10:34:30 +0100

    hello world compiles again

--- examples/hello.nim
+++ examples/hello.nim
@@ -2,24 +2,28 @@
 import ui, urhomain, processutils, color, urstr, stringHash, variant, text,
   uielement
 
+# enable auto-deref for this module:
+{.experimental.}
+
 proc onKeyDown(userData: pointer; eventType: StringHash;
-               eventData: pointer) {.cdecl.} =
+               eventData: var VariantMap) {.cdecl.} =
   closeUrho3D()
 
 proc main =
   parseArguments()
 
   openUrho3D(false)
-  registerEvent(onKeyDown, nil, "KeyDown") #E_KEYDOWN)
+
+  subscribeToEvent("KeyDown", onKeyDown, nil)
 
   let text = cnew constructText(getContext())
-  text[].setText("Hello Cruel World!")
-  text[].setColor(color.WHITE)
-  text[].setFont(getFont("Fonts/BlueHighway.ttf"), 42)
-  text[].setHorizontalAlignment(HA_CENTER)
-  text[].setVerticalAlignment(VA_CENTER)
+  text.setText("Hello Cruel World!")
+  text.setColor(color.WHITE)
+  text.setFont(getFont("Fonts/BlueHighway.ttf"), 42)
+  text.setHorizontalAlignment(HA_CENTER)
+  text.setVerticalAlignment(VA_CENTER)
 
-  getSubsystemUI()[].getRoot()[].addChild(text)
+  getSubsystemUI().getRoot().addChild(text)
   let exitCode = runMainLoop()
   quit exitCode
 
