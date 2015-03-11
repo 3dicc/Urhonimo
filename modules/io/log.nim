f6aaaa6e238eefae7a5b01927ba1aaba7f35cc5d
Araq <rumpf_a@web.de>
2015-03-11 01:25:03 +0100

    log module compiles and works

--- modules/io/log.nim
+++ modules/io/log.nim
@@ -1,29 +1,19 @@
 
 
-import 
+import
   list, mutex, UrObject, stringUtils, urstr, ptrs, stringHash
 
 
-var LOG_RAW* {.importc: "LOG_RAW", header: "Log.h".}: cint = - 1
-
-
-var LOG_DEBUG* {.importc: "LOG_DEBUG", header: "Log.h".}: cint = 0
-
-
-var LOG_INFO* {.importc: "LOG_INFO", header: "Log.h".}: cint = 1
-
-
-var LOG_WARNING* {.importc: "LOG_WARNING", header: "Log.h".}: cint = 2
-
-
-var LOG_ERROR* {.importc: "LOG_ERROR", header: "Log.h".}: cint = 3
-
-
-var LOG_NONE* {.importc: "LOG_NONE", header: "Log.h".}: cint = 4
+var LOG_RAW* {.importcpp: "Urho3D::LOG_RAW", header: "Log.h".}: cint #= - 1
+var LOG_DEBUG* {.importcpp: "Urho3D::LOG_DEBUG", header: "Log.h".}: cint #= 0
+var LOG_INFO* {.importcpp: "Urho3D::LOG_INFO", header: "Log.h".}: cint #= 1
+var LOG_WARNING* {.importcpp: "Urho3D::LOG_WARNING", header: "Log.h".}: cint #= 2
+var LOG_ERROR* {.importcpp: "Urho3D::LOG_ERROR", header: "Log.h".}: cint #= 3
+var LOG_NONE* {.importcpp: "Urho3D::LOG_NONE", header: "Log.h".}: cint #= 4
 
 discard "forward decl of File"
-type 
-  StoredLogMessage* {.importc: "Urho3D::StoredLogMessage", header: "Log.h".} = object 
+type
+  StoredLogMessage* {.importcpp: "Urho3D::StoredLogMessage", header: "Log.h".} = object
     message* {.importc: "message_".}: UrString
     level* {.importc: "level_".}: cint
     error* {.importc: "error_".}: bool
@@ -34,8 +24,8 @@ proc constructStoredLogMessage*(): StoredLogMessage {.
 proc constructStoredLogMessage*(message: UrString; level: cint; error: bool): StoredLogMessage {.
     importcpp: "Urho3D::StoredLogMessage(@)", header: "Log.h".}
 
-type 
-  Log* {.importc: "Urho3D::Log", header: "Log.h".} = object of UrObject
+type
+  Log* {.importcpp: "Urho3D::Log", header: "Log.h".} = object of UrObject
     logMutex* {.importc: "logMutex_".}: Mutex
     threadMessages* {.importc: "threadMessages_".}: List[StoredLogMessage]
     logFile* {.importc: "logFile_".}: SharedPtr[File]
@@ -46,35 +36,35 @@ type
     quiet* {.importc: "quiet_".}: bool
 
 
-proc getType*(this: Log): StringHash {.noSideEffect, 
+proc getType*(this: Log): StringHash {.noSideEffect,
     importcpp: "GetType", header: "Log.h".}
-proc getBaseType*(this: Log): StringHash {.noSideEffect, 
+proc getBaseType*(this: Log): StringHash {.noSideEffect,
     importcpp: "GetBaseType", header: "Log.h".}
-proc getTypeName*(this: Log): UrString {.noSideEffect, 
+proc getTypeName*(this: Log): UrString {.noSideEffect,
     importcpp: "GetTypeName", header: "Log.h".}
 proc getTypeStatic*(): StringHash {.
     importcpp: "Urho3D::Log::GetTypeStatic(@)", header: "Log.h".}
 proc getTypeNameStatic*(): UrString {.
     importcpp: "Urho3D::Log::GetTypeNameStatic(@)", header: "Log.h".}
-proc constructLog*(context: ptr Context): Log {.importcpp: "Urho3D::Log(@)", 
+proc constructLog*(context: ptr Context): Log {.importcpp: "Urho3D::Log(@)",
     header: "Log.h".}
 proc destroyLog*(this: var Log) {.importcpp: "#.~Log()", header: "Log.h".}
-proc open*(this: var Log; fileName: UrString) {.importcpp: "Open", 
+proc open*(this: var Log; fileName: UrString) {.importcpp: "Open",
     header: "Log.h".}
 proc close*(this: var Log) {.importcpp: "Close", header: "Log.h".}
-proc setLevel*(this: var Log; level: cint) {.importcpp: "SetLevel", 
+proc setLevel*(this: var Log; level: cint) {.importcpp: "SetLevel",
     header: "Log.h".}
-proc setTimeStamp*(this: var Log; enable: bool) {.importcpp: "SetTimeStamp", 
+proc setTimeStamp*(this: var Log; enable: bool) {.importcpp: "SetTimeStamp",
     header: "Log.h".}
-proc setQuiet*(this: var Log; quiet: bool) {.importcpp: "SetQuiet", 
+proc setQuiet*(this: var Log; quiet: bool) {.importcpp: "SetQuiet",
     header: "Log.h".}
-proc getLevel*(this: Log): cint {.noSideEffect, importcpp: "GetLevel", 
+proc getLevel*(this: Log): cint {.noSideEffect, importcpp: "GetLevel",
                                   header: "Log.h".}
-proc getTimeStamp*(this: Log): bool {.noSideEffect, importcpp: "GetTimeStamp", 
+proc getTimeStamp*(this: Log): bool {.noSideEffect, importcpp: "GetTimeStamp",
                                       header: "Log.h".}
-proc getLastMessage*(this: Log): UrString {.noSideEffect, 
+proc getLastMessage*(this: Log): UrString {.noSideEffect,
     importcpp: "GetLastMessage", header: "Log.h".}
-proc isQuiet*(this: Log): bool {.noSideEffect, importcpp: "IsQuiet", 
+proc isQuiet*(this: Log): bool {.noSideEffect, importcpp: "IsQuiet",
                                  header: "Log.h".}
 proc write*(level: cint; message: UrString) {.
     importcpp: "Urho3D::Log::Write(@)", header: "Log.h".}
